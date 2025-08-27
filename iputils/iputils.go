b4b4f326ef6a5af267eb8332a61c44f555bfedfe
similadayo <oladayoilesanmi@gmail.com>
2025-08-27 10:54:27 +0100

    Add Broadcast function to calculate broadcast address for IP networks

--- iputils/iputils.go
+++ iputils/iputils.go
@@ -1,1 +1,18 @@
 package iputils
+
+import "net"
+
+// Broadcast calculates the broadcast address for a given IP network.
+func Broadcast(ipnet *net.IPNet) net.IP {
+	ip := ipnet.IP.To4()
+	if ip == nil {
+		return nil // Not an IPv4 address
+	}
+
+	mask := ipnet.Mask
+	broadcast := make(net.IP, len(ip))
+	for i := 0; i < len(ip); i++ {
+		broadcast[i] = ip[i] | ^mask[i]
+	}
+	return broadcast
+}
