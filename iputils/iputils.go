fc0345bad808c46b79a4844b816de64a95602440
similadayo <oladayoilesanmi@gmail.com>
2025-08-27 10:59:21 +0100

    Implement IncrementIP function to increment an IP address by a specified value

--- iputils/iputils.go
+++ iputils/iputils.go
@@ -16,3 +16,20 @@ func Broadcast(ipnet *net.IPNet) net.IP {
 	}
 	return broadcast
 }
+
+// IncrementIP increments an IP address by a given value.
+func IncrementIP(ip net.IP, increment int) net.IP {
+	ip = ip.To4()
+	if ip == nil {
+		return nil // Not an IPv4 address
+	}
+
+	val := uint64(ip[0])<<24 | uint64(ip[1])<<16 | uint64(ip[2])<<8 | uint64(ip[3])
+	val += uint64(increment)
+	newIP := make(net.IP, 4)
+	newIP[0] = byte((val >> 24) & 0xFF)
+	newIP[1] = byte((val >> 16) & 0xFF)
+	newIP[2] = byte((val >> 8) & 0xFF)
+	newIP[3] = byte(val & 0xFF)
+	return newIP
+}
